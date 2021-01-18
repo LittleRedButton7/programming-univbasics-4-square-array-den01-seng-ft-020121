b3554cd3091b0ab068a30c3d9846ab25f50f1b0c
Marissa Nolan <ritz1484@cs.com>
2021-01-18 20:35:39 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -12,3 +12,18 @@ square_array([1,2,3])
 # takes each element in an array
 # squares that element
 # retrns new array containing the squared elements
+
+
+#define a method that receives an array and returns a new array with the values squared in place
+
+def squared_array(array)
+
+#Define local array for output
+
+#loop over each value of the array
+
+# for each value square that value and place into local array for aoutpu
+
+# after loop return local array
+
+
